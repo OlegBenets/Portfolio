6739d24428cdc2b8e6e2ab9301132347cac6e5ff
Oleg Benets <oleg.benets@web.de>
2024-07-07 12:41:54 +0200

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent, 
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
