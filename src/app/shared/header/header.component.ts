96f6a68c94f1c2787373664ece75358e5b60ce54
Oleg Benets <oleg.benets@web.de>
2024-07-18 12:07:29 +0200

    added burger-menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit{
+
+  isBurgerMenuVisible: boolean = false;
+  currentImage: string = './../../../assets/img/burger menu.png';
+  images: string[] = [
+    './../../../assets/img/Property 1=Transition.png',
+    './../../../assets/img/Property 1=close medium.png',
+    './../../../assets/img/Property 1=CLOSE FINAL.png'
+  ];
+
+  animationInterval: any;
+
 
   ngOnInit(): void {
     this.animateHeader();
@@ -22,5 +34,39 @@ export class HeaderComponent implements OnInit{
       {y: '-100%', opacity: 0},
       {y: '0%', opacity: 1, duration: 1}
     )
+  }
+
+  toggleBurgerMenu() {
+    this.isBurgerMenuVisible = !this.isBurgerMenuVisible;
+      
+    if (this.isBurgerMenuVisible) {
+      this.animateImages();
+    } else {
+      this.reverseAnimationImages();
+    }
+  }
+
+  animateImages() {
+    let index = 0;
+    clearInterval(this.animationInterval);
+    this.animationInterval = setInterval(() => {
+      this.currentImage = this.images[index];
+      index = (index + 1) % this.images.length;
+      if(!this.isBurgerMenuVisible || index === 0) {
+        clearInterval(this.animationInterval);
+      }
+    }, 80);
+  }
+
+  reverseAnimationImages() {
+    let index = this.images.length - 1;
+    clearInterval(this.animationInterval);
+    this.animationInterval = setInterval(() => {
+      this.currentImage = this.images[index];
+      index--;
+      if(index < 0 || this.isBurgerMenuVisible) {
+        clearInterval(this.animationInterval);
+      }
+    }, 80);
   }
 }
