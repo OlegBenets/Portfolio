a6e222c29e0edaede7c33684f4f0eae67460ce73
Oleg Benets <oleg.benets@web.de>
2024-07-26 19:09:51 +0200

    changed checkbox style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -10,7 +11,7 @@ gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, TranslateModule],
+  imports: [FormsModule, TranslateModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
